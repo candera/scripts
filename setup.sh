e3a3675507e7effd7913b95b68b1d930ce927987
Craig Andera <candera@wangdera.com>
2011-05-13 09:40:00 -0400

    Added git setup of colors, name and email.

--- setup.sh
+++ setup.sh
@@ -18,3 +18,8 @@ source ~/.bashrc
 curl -k -o ~/bin/lein https://github.com/technomancy/leiningen/raw/stable/bin/lein 
 chmod u+x ~/bin/lein
 lein self-install
+
+# Set up git
+git config --global color.ui auto
+git config --global user.name "Craig Andera"
+git config --global user.email candera@wangdera.com
